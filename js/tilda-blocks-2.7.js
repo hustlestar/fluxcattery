ea479c059954cc9e833ca2338e032daa07bf0886
Jack Malaschitsky <yauheni.malashchytski@gmail.com>
2021-01-31 12:10:09 +0300

    new v

--- js/tilda-blocks-2.7.js
+++ js/tilda-blocks-2.7.js
@@ -283,6 +283,15 @@ function t480_setHeight(recid) {
     imgwrapper.css({'height':((imgwidth/ratio)+'px')});
   }
 } 
+function t537_setHeight(recid) {
+  var t537__el=$("#rec"+recid),
+      t537__image = t537__el.find(".t537__bgimg:first"),
+      t537__width = t537__image.attr("data-image-width"),
+      t537__height = t537__image.attr("data-image-height"),
+      t537__ratio = t537__height/t537__width,
+      t537__padding = t537__ratio*100;	  
+  $("#rec"+recid+" .t537__bgimg").css("padding-bottom",t537__padding+"%");  
+} 
 function t670_init(recid) {
     t670_imageHeight(recid);
     t670_show(recid);
